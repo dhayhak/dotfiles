71aa274bf750dcf0b730940b8179e1dfae9763f8
Andrew Lavers <andrew@smooch.io>
2016-06-15 13:30:55 -0400

    npm global modules

--- install/misc.sh
+++ install/misc.sh
@@ -1,4 +1,7 @@
 # thefuck https://github.com/nvbn/thefuck
 wget -O - https://raw.githubusercontent.com/nvbn/thefuck/master/install.sh | sh - && $0
 
+# Global npm packages
+npm install -g grunt-cli node-inspector mocha karma karma-cli phantomjs karma-phantomjs-launcher eslint eslint-plugin-react babel-eslint
+
 
